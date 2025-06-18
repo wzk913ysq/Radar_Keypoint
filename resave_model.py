5f248163eeb53e53aaaea08dd192938ad79f6d5d
Wen2001 <wen20010913@163.com>
2025-06-18 15:51:41 +0800

    Delete resave_model.py

--- resave_model.py
+++
@@ -1,9 +0,0 @@
-import torch
-import sys
-from models.box_pre_net import *
-
-a = Net1(BasicBlock, [2, 2, 2, 2])	
-a = torch.nn.DataParallel(a).cuda()
-b = torch.load('state.pth')
-a.load_state_dict(b)
-torch.save(a, 'area_pre3.pth')
